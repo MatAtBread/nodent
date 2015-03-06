22e56d1049a4b2b97c140ea26ed9045873bf021d
Matthew W <matthew.woolf@mailonline.co.uk>
2015-03-06 18:36:28 +0000

    Update performance tests Correct paths used by test runner (tests/index.js)

--- tests/index.js
+++ tests/index.js
@@ -2,7 +2,7 @@
 
 /* Run all the scripts in ./tests compiled for ES7 and Promises */
 var fs = require('fs') ;
-var nodent = require('../nodent')() ;
+var nodent = require('../nodent')({use:["map"],log:function(msg){ msgs.push(msg) }}) ;
 var Promise = nodent.Thenable ;
 
 async function sleep(t) {
@@ -15,13 +15,14 @@ if (global.Promise) {
 }
 
 providers = providers.concat([
-    {name:'nodent-ES7',p:null},
- 	{name:'nodent-Thenable',p:nodent.Thenable},
+    {name:'nodent-es7',p:null},
+ 	{name:'nodent.Thenable',p:nodent.Thenable},
 	{name:'bluebird',p:require('bluebird')},
 	{name:'rsvp',p:require('rsvp').Promise},
 	{name:'when',p:require('when').promise}
 ]) ;
 
+var msgs = [] ;
 var showOutput = false ;
 var idx = 3 ;
 if (process.argv[3]=='--out') {
@@ -29,19 +30,27 @@ if (process.argv[3]=='--out') {
 	idx += 1 ;
 	providers = [providers[0]] ;
 }
-var tests = process.argv.length>idx ? process.argv.slice(idx):fs.readdirSync('./tests') ;
+
+function pad(s) {
+	return (s+"                ").substring(0,24)
+}
+
+var tests = process.argv.length>idx ? 
+		process.argv.slice(idx):
+		fs.readdirSync('./tests').map(function(fn){ return './tests/'+fn}) ;
 
 async function runTests() {
 	for (var j=0; j<tests.length; j++) {
 		var test = tests[j] ;
-		if (test=="index.js" || !test.match(/.*\.js$/))
+		if (test.match(/tests\/index.js$/) || !test.match(/.*\.js$/))
 			continue ;
-		var info = [(test+"        ").substring(0,15)] ;
+		var info = [pad(test)] ;
 		var failed = false ;
+		msgs = [] ;
 		for (var i=0; i<providers.length; i++) {
 			var promise = providers[i] ;
 	
-			var code = fs.readFileSync('./tests/'+test).toString() ;
+			var code = fs.readFileSync(test).toString() ;
 			var pr = nodent.compile(code,test,3,{es7:true,promises:!!promise.p}) ;
 			var m = {} ;
 			var fn = new Function("module","require","Promise",pr.code) ;
@@ -62,10 +71,14 @@ async function runTests() {
 					info.push([promise.name,t+"ms"]) ;
 				}
 			} catch (ex) {
-				info.push([promise.name,"CATCH",ex]) ;
+				info.push([promise.name,"EX"]) ;
+				msgs.push(promise.name+" EX:"+ex.message+"\n"+ex.stack) ;
 			}
 		}
 		console.log(info.join("\t"));
+		if (msgs.length)
+			msgs.forEach(function(m){ console.log("  "+m)});
+
 	//	if (failed)
 	//		console.log(failed) ;
 	}
