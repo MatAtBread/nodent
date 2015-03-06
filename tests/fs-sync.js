9287320d55a2b524c61a00a2b8eac3bc815b7e38
mat <bread@mailed.me.uk>
2015-03-06 19:03:53 +0000

    Add sync fs test for speed comparison

---
+++ tests/fs-sync.js
@@ -0,0 +1,19 @@
+var fs = require('../nodent').asyncify(Promise)(require('fs')) ;
+
+var n = fs.readdirSync('.').length ;
+
+function test(dir) {
+	var files ;
+	for (var i=0; i<100; i++) {
+		files = fs.readdirSync(dir) ;
+		for (var j=0; j<files.length; j++) {
+			var stat = fs.statSync(dir+"/"+files[j]) ;
+		}
+	}
+	return files.length ;
+}
+
+module.exports = async function() {
+	return test('.')==n ;
+}
+
