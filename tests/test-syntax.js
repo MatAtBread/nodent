caf678851af736a3de9ea09b89cb7468e2368b8d
Matthew W <matthew.woolf@mailonline.co.uk>
2015-10-23 15:13:10 +0100

    Fix shorthand property case and more precendence issues

--- tests/test-syntax.js
+++ tests/test-syntax.js
@@ -6,47 +6,59 @@ var fs = require('fs');
 
 var n = 0 ;
 var opts = {parser:{sourceType:'module',onComment:null}} ;
+
+var pass = ['./tests/semantics/asyncify.js'] ;
 var pass = fs.readdirSync('./tests/syntax').map(function(fn){ return './tests/syntax/'+fn})
-	.concat(fs.readdirSync('./tests/semantics').map(function(fn){ return './tests/semantics/'+fn}))
-	.map(function(fn){
+	.concat(fs.readdirSync('./tests/semantics').map(function(fn){ return './tests/semantics/'+fn})) ;
+
+pass = pass.map(function(fn){
 	var code = fs.readFileSync(fn).toString() ;
 	try {
+		var r = {name:fn, pass:false, toString:function(){
+			return this.name+" pass:"+this.pass+"\nsource:"+this.source+"\ninput :"+this.inputs+"\noutput:"+this.output+"\nerror :"+this.error
+		}} ;
+		r.source = code.replace(/\s+/g," ") ;
 		var ci = nodent.prettyPrint(nodent.parse(code,"",null,opts),opts) ;
-		var co = nodent.prettyPrint(nodent.parse(ci.code,"",null,opts)) ;
-		ci.code = ci.code.replace(/\s+/g," ") ;
-		co.code = co.code.replace(/\s+/g," ") ;
+		r.inputs = ci.code.replace(/\s+/g," ") ;
+		var co = nodent.prettyPrint(nodent.parse(r.inputs,"",null,opts)) ;
+		r.output = co.code.replace(/\s+/g," ") ;
 		var eq = eqTree(ci.ast,co.ast) ;
-		if (eq && ci.code==co.code) {
+		if (eq && r.inputs==r.output) {
 			n += 1 ;
 			return {name:fn,pass:true} ;
 		} else {
-			return {name:fn,pass:false} ;
+			return r ;
 		}
 	} catch (ex) {
-		ex.message = fn+": "+ex.message ;
-		throw ex ;
+//		throw ex ;
+		r.error = ex.message ;
+		return r ; 
 	}
 }) ;
 if (n===pass.length)
 	console.log("Syntax check - pass "+n+" of "+pass.length) ;
-else
-	console.log("Syntax check - FAIL "+(pass.length-n)+" of "+pass.length+". Errors:\n",pass.filter(function(p){ return !p.pass})) ;
+else {
+	console.log("Syntax check - Errors\n",pass.filter(function(p){ return !p.pass}).join("\n")) ;
+	console.log("Syntax check - FAIL "+(pass.length-n)+" of "+pass.length) ;
+}
 
 function locations(k) {
 	return ['end','start','loc'].indexOf(k)<0 ;
 }
 
-function eqTree(a,b) {
-	debugger ;
+function eqTree(a,b,p) {
+	if (!p) p = "" ;
 	var ka = Object.keys(a).filter(locations).sort() ;
 	var kb = Object.keys(b).filter(locations).sort() ;
 	if (ka.length != kb.length)
-		return false ;
+		throw new Error("length("+ka.length+","+kb.length+") "+p) ;
+//		return false ;
 	for (var i=0;i<ka.length;i++)
 		if (ka[i] != kb[i] || typeof a[ka[i]] != typeof b[kb[i]])
-			return false ;
+			throw new Error("key("+ka[i]+","+kb[i]+") "+p) ;
+//			return false ;
 	for (var i=0;i<ka.length;i++)
 		if (typeof a[ka[i]] === 'object' && a[ka[i]])
-			return eqTree(a[ka[i]],b[kb[i]]) ;
+			eqTree(a[ka[i]],b[kb[i]],p+" > "+ka[i]) ;
 	return true ;
 }
