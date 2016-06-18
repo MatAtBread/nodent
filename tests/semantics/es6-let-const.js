16c45f2d58174b23931f2f3eb1da943798ca0548
Matthew W <matthew.woolf@mailonline.co.uk>
2016-06-18 18:12:41 +0100

    Implement hoisting using "Living Web" defintion of "const" (i.e. ES6 in strict mode, otherwise function-scoped in sloppy)

--- tests/semantics/es6-let-const.js
+++ tests/semantics/es6-let-const.js
@@ -2,12 +2,42 @@ async function inc(m) {
     return m+1 ;
 }
 
-async function test(){
+async function test1(){
+    "use sloppy";
     const p = await inc(0) ;
-    let q = p ;
+    var q = p ;
+    if (true) {
+        var block_var = 0 ;
+        const block_const = 0 ;
+        let block_let = 0 ;
+    }
     function y() {
+        var y_var = 0 ;
+        const y_const = 0 ;
+        let y_let = 0 ;
         return p ;
     }
-    return y() === q;
+    return (y() === q);
 }
-module.exports = test ;
+
+async function test2(){
+    "use strict";
+    const p = await inc(0) ;
+    var q = p ;
+    if (true) {
+        var block_var = 0 ;
+        const block_const = 0 ;
+        let block_let = 0 ;
+    }
+    function y() {
+        var y_var = 0 ;
+        const y_const = 0 ;
+        let y_let = 0 ;
+        return p ;
+    }
+    return (y() === q);
+}
+
+module.exports = async function() {
+    return await test1() && await test2() ;
+}
