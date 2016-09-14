d5759efd427c3722dc7a0124f4db7486c8f60f7a
Matthew W <matthew.woolf@mailonline.co.uk>
2016-09-14 10:28:02 +0100

    Fix loop early-exit labels Fix missing 'async' in dual-for-of-const (NB: this introduced another failure case, for-in/of with await inside a non-async function, which should work)

--- tests/semantics/dual-for-of-const-destructure.js
+++ tests/semantics/dual-for-of-const-destructure.js
@@ -2,7 +2,7 @@
 
 async function nop(x) { return x }
 
-function b() {
+async function b() {
     var n = 0 ;
     for (const {ans:a, z} of [{ans:5,z:10},{ans:3,z:20},{ans:1,z:30}]) {
         n += await nop(a*z) ;
