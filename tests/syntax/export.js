6448b592ce18a387e027230a9c918efcdc5050df
Matthew W <matthew.woolf@mailonline.co.uk>
2016-09-23 12:13:59 +0100

    Update to Acorn 4
    Move from acorn-es7-plugin to acorn-nodent-plugin
    Update exports.js test to be ES7 compliant (old test passed even tho it
    was invalid JS, now caught by acorn 4)
    Update watch-comp

--- tests/syntax/export.js
+++ tests/syntax/export.js
@@ -1,10 +1,10 @@
 export * from "module";
 export {name} from "module";
 export {a as b, c as d} from "module";
-export {a as b, c as d};
+export {a as b1, c as d1};
 export {};
-export default a = 42;
 export var a = 42;
-export let a = 42;
+export let a1 = 42;
 export function test() {}
 export default function test() {}
+export async function atest() {}
