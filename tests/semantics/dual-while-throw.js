8e1fa921b76407f9723ade4d8116ffaad7a59c62
Matthew W <matthew.woolf@mailonline.co.uk>
2016-09-26 23:52:09 +0100

    Handle synchronous exceptions in loop trampolines.
    Add test dual-while-throw
    Thanks for https://github.com/Arnavion for finding this one

---
+++ tests/semantics/dual-while-throw.js
@@ -0,0 +1,18 @@
+async function foo() {
+    JSON.parse("*");
+}
+
+async function bar() { }
+
+async function baz() {
+    do await bar();
+    while(false);
+    await foo() ;
+}
+
+try {
+    await baz() ;
+    return "baz" ;
+} catch (ex) {
+    return ex.message ;
+}
