fcc530eec3bfce879ea15b1fb8749215a1841ebe
Matthew W <matthew.woolf@mailonline.co.uk>
2016-09-26 22:41:39 +0100

    Test es7-lazy in interop tests Test async and sync loops in interop tests

--- tests/semantics/es6-interop.js
+++ tests/semantics/es6-interop.js
@@ -4,13 +4,19 @@ var gen = [
 	require('./codegen/cg-generators.js')
 ];
 
-function* gen(){}
+function* doWeKnowGenerators(){}
 
 async function inter(i,j) {
-	return (gen[j].name == await gen[j].call() && await gen[j].call() == await gen[i].consume(gen[j].call))?1:100 ;
+ 	return (gen[j].name == await gen[j].call() && await gen[j].call() == await gen[i].consume(gen[j].call))?1:100 ;
 }
 
 async function test() {
+    for (var q=0; q<gen.length*gen.length*2; q++) {
+        if (!q%gen.length)
+            await breathe() ;
+        await inter(q%gen.length,(q/gen.length)%gen.length|0) ;
+    }
+        
 	var single = (await inter(0,1) +
 		await inter(0,2) +
 		await inter(1,0) +
