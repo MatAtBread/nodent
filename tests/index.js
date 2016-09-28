1230cccb77b83136002242804b6cfca0078273f9
Matthew W <matthew.woolf@mailonline.co.uk>
2016-09-28 14:53:17 +0100

    Partial fix for fast-async-13: block scoped for-loop declarations incorrectly scoped

--- tests/index.js
+++ tests/index.js
@@ -195,17 +195,24 @@ files.forEach(function (n) {
               opts.es7 = true;
         }
         types[type] = Object.keys(opts).toString() ;
-        var pr, tCompiler = process.hrtime();
-        pr = nodent.compile(forceStrict + code, n, opts).code;
-
-        tTotalCompilerTime += time(tCompiler);
         try {
-            test[i].fn[type] = new Function("module", "require", "Promise", "__unused", "nodent", "DoNotTest", pr);
-        } catch (ex) {
-            if (!compileException) {
-                console.warn(test[i].name+(" not supported by V8 "+process.versions.v8+" (try a later version of nodejs): ").yellow+ex.message.red) ;
-                compileException = true ;
+            var pr, tCompiler = process.hrtime();
+            pr = nodent.compile(forceStrict + code, n, opts).code;
+            tTotalCompilerTime += time(tCompiler);
+            try {
+                test[i].fn[type] = new Function("module", "require", "Promise", "__unused", "nodent", "DoNotTest", pr);
+            } catch (ex) {
+                if (!compileException) {
+                    console.warn(test[i].name+(" not supported by V8 "+process.versions.v8+" (try a later version of nodejs): ").yellow+ex.message.red) ;
+                    compileException = true ;
+                }
+                test[i].fn[type] = function(m) {
+                    m.exports = DoNotTest ;
+                }
             }
+        } catch (ex) {
+            console.warn(test[i].name+(" nodent failed to compile - FAIL ").yellow+ex.message.red) ;
+            compileException = true ;
             test[i].fn[type] = function(m) {
                 m.exports = DoNotTest ;
             }
@@ -272,7 +279,7 @@ async function runTest(test, provider, type) {
 
 try {
     var result, byType = {}, byProvider = {}, byTest = {}, table = [], fails = [], tMedian = 0, nMedian = 0 ;
-    for (var i = 0;i < test.length; i++) {
+    for (i = 0;i < test.length; i++) {
         var benchmark = null;
         for (var j = 0;j < providers.length; j++) {
             try {
