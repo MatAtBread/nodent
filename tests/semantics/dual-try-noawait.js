4f6e7253292c79392e374f24e45ecf74170d1a45
Matthew W <matthew.woolf@mailonline.co.uk>
2016-10-06 16:50:12 +0100

    Merge changes fixing `finally { return/throw... }` in synchronous code

---
+++ tests/semantics/dual-try-noawait.js
@@ -0,0 +1,45 @@
+'use nodent';
+
+async function nop(x) { return x }
+async function fn1() {
+    try {
+        throw new Error();
+    }
+    catch (error) {
+        return 'caught';
+    }
+    finally {
+        return 'ok';
+    }
+}
+
+async function fn2() {
+    try {
+        return '1';
+    }
+    finally {
+        return '2';
+    }
+}
+
+async function fn(a) {
+    try {
+        if (a&1)
+            return '1';
+        throw new Error() ;
+    }
+    catch (ex) {
+        if (a&2)
+            return '2' ;
+        return '4' ;
+    }
+    finally {
+        if (a&4)
+            return '3' ;
+    }
+}
+
+var s = "" ;
+for (var i=0; i<8; i++)
+    s += await fn(i) ;
+return s+await fn1()+":"+await fn2();
