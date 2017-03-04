4928f4ed4ca9dad1a4d05a66dff31f6ccf7af260
MatAtBread <bread@mailed.me.uk>
2017-03-04 11:41:37 +0000

    Fix https://github.com/MatAtBread/nodent/issues/78

--- tests/semantics/es6-args.js
+++ tests/semantics/es6-args.js
@@ -29,12 +29,28 @@ class Q {
 	}
 }
 
-async function test() {
+async function test1() {
 	var args = "the quick brown fox jumps over".split(" ") ;
 	var r = [await a.apply(null,args),await b.apply(null,args),
 	         await c.a.apply(null,args), await c.b.apply(null,args),
 	         await new q().a.apply(null,args), await new Q().b.apply(null,args)] ;
 	return (r.join(" ")===args.join(" ")) ;
+}
+
+async function test2() {
+	var foo = async function() {
+		var bar1 = async function() { return  arguments[0] } ;
+		var bar2 = async () => { return  arguments[0] } ;
+		var bar3 = async () => arguments[0] ;
+
+	  return await bar1(arguments[0])===123 && await bar2(arguments[0])===123 && await bar3(arguments[0])===123 ;
+	}
+
+	return await foo(123) ;
+}
+
+async function test() {
+	return await test1() && await test2() ;
 }
 
 module.exports = test ;
